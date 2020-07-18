c7a021e05c4f649b2210e2901b9386702ab38a3b
Joshua Gallagher <joshgallagher24@gmail.com>
2020-07-18 21:56:43 +0100

    Added Tailwindcss (#1)
    * Added tailwindcss
    
    * Added postcss config
    
    * Configured tailwind css import
    
    * Added base tailwindcss config
    
    * Configured custom tailwindcss forms

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="bg-blue-500">
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -7,6 +7,8 @@
     <router-view />
   </div>
 </template>
+
+<style src="@/assets/styles/app.css" />
 
 <style>
 #app {
