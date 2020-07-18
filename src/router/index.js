d0ebdb044da6da70c576c85ae69446c1da33bb4d
Josh <joshgallagher24@gmail.com>
2020-07-19 00:34:55 +0100

    Setup route for OpenID callback and included other callback components

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+
 import Home from "../views/Home.vue";
+import Callback from "../views/auth/Callback.vue";
+import PopupCallback from "../views/auth/PopupCallback.vue";
+import ErrorCallback from "../views/auth/ErrorCallback.vue";
 
 Vue.use(VueRouter);
 
@@ -18,6 +22,24 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/callback",
+    name: "Callback",
+    component: Callback
+  },
+  {
+    path: "/popup-callback",
+    name: "PopupCallback",
+    component: PopupCallback
+  },
+  {
+    path: "/callback-error",
+    name: "ErrorCallback",
+    component: ErrorCallback,
+    meta: {
+      isPublic: true
+    }
   }
 ];
 
