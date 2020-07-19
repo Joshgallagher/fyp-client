78854c284940839437243874610108373ca837f1
Josh <joshgallagher24@gmail.com>
2020-07-19 20:25:48 +0100

    Setup SignUp component logic with error handling

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -4,37 +4,76 @@
       <h1 class="text-teal-400 font-bold text-xl">Pondr.</h1>
     </header>
     <main class="flex-grow flex justify-center items-center">
-      <div class="grid grid-cols-12">
+      <div class="grid grid-cols-12 min-w-full">
         <h1 class="col-start-5 col-end-9 text-4xl font-bold">
           Join Pondr Today.
         </h1>
         <p class="col-start-5 col-end-9 text-xl mb-8 text-gray-700">
-          Share your thoughts with the world.
+          Share your thoughts with the world 🌎
         </p>
-        <form class="flex flex-col col-start-5 col-end-9">
+        <form
+          class="flex flex-col col-start-5 col-end-9"
+          @submit.prevent="submit"
+        >
           <label class="block mb-4">
-            <span class="text-gray-700">Name</span>
+            <span
+              class="text-gray-700"
+              :class="{ 'text-red-600': this.error.field === 'name' }"
+              >Name</span
+            >
             <input
               type="text"
               class="form-input mt-1 block w-full"
+              :class="{ 'border-red-600': this.error.field === 'name' }"
               placeholder="Jane Doe"
+              v-model="form.name"
             />
+            <p
+              class="mt-2 ml-2 text-red-600 text-sm"
+              v-if="this.error.field === 'name'"
+            >
+              {{ this.error.message }}
+            </p>
           </label>
           <label class="block mb-4">
-            <span class="text-gray-700">Email</span>
+            <span
+              class="text-gray-700"
+              :class="{ 'text-red-600': this.error.field === 'email' }"
+              >Email</span
+            >
             <input
               type="email"
               class="form-input mt-1 block w-full"
+              :class="{ 'border-red-600': this.error.field === 'email' }"
               placeholder="jane.doe@example.com"
+              v-model="form.email"
             />
+            <p
+              class="mt-2 ml-2 text-red-600 text-sm"
+              v-if="this.error.field === 'email'"
+            >
+              {{ this.error.message }}
+            </p>
           </label>
           <label class="block mb-8">
-            <span class="text-gray-700">Password</span>
+            <span
+              class="text-gray-700"
+              :class="{ 'text-red-600': this.error.field === 'password' }"
+              >Password</span
+            >
             <input
               type="password"
               class="form-input mt-1 block w-full"
+              :class="{ 'border-red-600': this.error.field === 'password' }"
               placeholder="******"
+              v-model="form.password"
             />
+            <p
+              class="mt-2 ml-2 text-red-600 text-sm"
+              v-if="this.error.field === 'password'"
+            >
+              {{ this.error.message }}
+            </p>
           </label>
           <input
             type="submit"
@@ -62,9 +101,23 @@
 
 <script>
 import { mapGetters, mapActions } from "vuex";
+import axios from "axios";
 
 export default {
   name: "SignUp",
+  data() {
+    return {
+      form: {
+        name: "",
+        email: "",
+        password: ""
+      },
+      error: {
+        field: null,
+        message: null
+      }
+    };
+  },
   computed: {
     ...mapGetters({
       authUser: "auth/oidcUser"
@@ -72,9 +125,27 @@ export default {
   },
   methods: {
     ...mapActions({
-      logIn: "auth/authenticateOidcPopup",
+      logIn: "auth/authenticateOidc",
       signOut: "auth/signOutOidc"
-    })
+    }),
+    async submit() {
+      let response = null;
+
+      Object.assign({ field: null, message: null }, this.error);
+
+      try {
+        response = await axios.post("user", this.form);
+
+        if (response.data.registered === true) {
+          this.logIn();
+        }
+      } catch ({ response }) {
+        if (response.status === 422) {
+          this.error.field = response.data.field;
+          this.error.message = response.data.message;
+        }
+      }
+    }
   }
 };
 </script>
