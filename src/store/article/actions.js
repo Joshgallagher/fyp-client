713121a81cd91548c1bf03b24383a15549c534f3
Josh <joshgallagher24@gmail.com>
2020-07-20 02:36:00 +0100

    Configured article store with Home component

--- src/store/article/actions.js
+++ src/store/article/actions.js
@@ -3,13 +3,13 @@ import * as mutationTypes from "./mutationTypes";
 import { getAll, getOne } from "./api";
 
 export const getArticles = async ({ commit }) => {
-  const data = getAll();
+  const data = await getAll();
 
   commit(mutationTypes.SET_ARTICLES, data);
 };
 
 export const getArticle = async ({ commit }, slug) => {
-  const data = getOne(slug);
+  const data = await getOne(slug);
 
   commit(mutationTypes.SET_ARTICLE, data.data);
 };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,70 +15,50 @@
       <div class="grid grid-cols-6 gap-8">
         <div class="col-start-2 col-end-6">
           <h1 class="font-bold text-4xl">
-            Latest
+            Home
           </h1>
           <p class="text-xl text-gray-700">
             Here you will find what people have been writing about...
           </p>
         </div>
-
-        <router-link
-          :to="{ name: 'home' }"
-          class="col-start-2 col-end-4 shadow-md rounded bg-white hover:shadow-xl transition linear duration-500"
-        >
-          <h1 class="pt-4 px-4 font-bold text-3xl mb-4">
-            Lorem ipsum dolor sit amet consectetur
-          </h1>
-          <p class="mb-6 px-4 text-gray-700">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
-            delectus rerum doloribus possimus.
-          </p>
-          <hr class="border-gray-200" />
-          <div class="flex flex-row px-4 py-4">
-            <img
-              src="https://uifaces.co/our-content/donated/T5rm0m7W.jpg"
-              alt="Avatar"
-              class="w-12 h-auto mr-4 rounded-full"
-            />
-            <span class="flex flex-col justify-between">
-              <p class="font-medium">Joshua Gallagher</p>
-              <span class="flex flex-row">
-                <time class="text-gray-700 text-sm mr-1">Jun 19</time>
-                <span class="text-gray-700 text-sm">·</span>
-                <p class="text-gray-700 text-sm ml-1">12 min read</p>
+        <template v-for="(article, i) in articles">
+          <router-link
+            :to="{ name: 'home' }"
+            :key="article.id"
+            class="col-span-2 shadow-md rounded bg-white hover:shadow-xl transition linear duration-500"
+            :class="{ 'col-start-2': i % 2 === 0, 'col-end-6': i % 2 !== 0 }"
+          >
+            <h1 class="pt-4 px-4 font-bold text-3xl mb-4">
+              {{ article.title }}
+            </h1>
+            <p class="mb-6 px-4 text-gray-700">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptatum delectus rerum doloribus possimus.
+            </p>
+            <hr class="border-gray-200" />
+            <router-link
+              :to="{ name: 'profile', params: { uuid: article.userId } }"
+              class="flex flex-row px-4 py-4 hover:bg-gray-100 transition linear duration-500"
+            >
+              <img
+                src="https://uifaces.co/our-content/donated/T5rm0m7W.jpg"
+                alt="Avatar"
+                class="w-12 h-auto mr-4 rounded-full"
+              />
+              <span class="flex flex-col justify-between">
+                <p class="font-medium">
+                  <span class="font-normal">Published by</span>
+                  {{ article.author }}
+                </p>
+                <span class="flex flex-row">
+                  <time class="text-gray-700 text-sm mr-1">Jun 19</time>
+                  <span class="text-gray-700 text-sm">·</span>
+                  <p class="text-gray-700 text-sm ml-1">12 min read</p>
+                </span>
               </span>
-            </span>
-          </div>
-        </router-link>
-
-        <router-link
-          :to="{ name: 'home' }"
-          class="col-start-4 col-end-6 shadow-md rounded bg-white hover:shadow-xl transition linear duration-500"
-        >
-          <h1 class="pt-4 px-4 font-bold text-3xl mb-4">
-            Lorem ipsum dolor sit amet consectetur
-          </h1>
-          <p class="mb-6 px-4 text-gray-700">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
-            delectus rerum doloribus possimus.
-          </p>
-          <hr class="border-gray-200" />
-          <div class="flex flex-row px-4 py-4">
-            <img
-              src="https://uifaces.co/our-content/donated/T5rm0m7W.jpg"
-              alt="Avatar"
-              class="w-12 h-auto mr-4 rounded-full"
-            />
-            <span class="flex flex-col justify-between">
-              <p class="font-medium">Joshua Gallagher</p>
-              <span class="flex flex-row">
-                <time class="text-gray-700 text-sm mr-1">Jun 19</time>
-                <span class="text-gray-700 text-sm">·</span>
-                <p class="text-gray-700 text-sm ml-1">12 min read</p>
-              </span>
-            </span>
-          </div>
-        </router-link>
+            </router-link>
+          </router-link>
+        </template>
       </div>
     </main>
     <footer class="py-6 px-12">
@@ -94,15 +74,20 @@ import { mapGetters, mapActions } from "vuex";
 
 export default {
   name: "Home",
+  mounted() {
+    this.getArticles();
+  },
   computed: {
     ...mapGetters({
-      authUser: "auth/oidcUser"
+      authUser: "auth/oidcUser",
+      articles: "article/articles"
     })
   },
   methods: {
     ...mapActions({
       logIn: "auth/authenticateOidcPopup",
-      signOut: "auth/signOutOidc"
+      signOut: "auth/signOutOidc",
+      getArticles: "article/getArticles"
     })
   }
 };
