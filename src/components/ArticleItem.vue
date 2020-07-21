bc2df35ff99e12445570c49bf52bcf16a2fbd9cc
Josh <joshgallagher24@gmail.com>
2020-07-21 04:29:34 +0100

    Added creating and removing of bookmarks

--- src/components/ArticleItem.vue
+++ src/components/ArticleItem.vue
@@ -15,34 +15,44 @@
     <hr class="border-gray-200" />
     <router-link
       :to="{ name: 'profile', params: { userId: article.userId } }"
-      class="flex flex-row px-4 py-4 hover:bg-gray-100 transition linear duration-500"
+      class="flex flex-row justify-between items-center px-4 py-4 hover:bg-gray-100 transition linear duration-500"
     >
-      <img
-        src="https://uifaces.co/our-content/donated/T5rm0m7W.jpg"
-        alt="Avatar"
-        class="w-12 h-auto mr-4 rounded-full"
-      />
-      <span class="flex flex-col justify-between">
-        <p class="font-medium">
-          <span class="font-normal">Published by</span>
-          {{ article.author }}
-        </p>
-        <span class="flex flex-row">
-          <time
-            :datetime="article.createdAt"
-            class="text-gray-700 text-sm mr-1"
-            >{{ createdAt }}</time
-          >
-          <span class="text-gray-700 text-sm mr-1">·</span>
-          <reading-time :content="article.body" />
+      <span class="flex flex-row">
+        <img
+          src="https://uifaces.co/our-content/donated/T5rm0m7W.jpg"
+          alt="Avatar"
+          class="w-12 h-auto mr-4 rounded-full"
+        />
+        <span class="flex flex-col justify-between">
+          <p class="font-medium">
+            <span class="font-normal">Published by</span>
+            {{ article.author }}
+          </p>
+          <span class="flex flex-row">
+            <time
+              :datetime="article.createdAt"
+              class="text-gray-700 text-sm mr-1"
+              >{{ createdAt }}</time
+            >
+            <span class="text-gray-700 text-sm mr-1">·</span>
+            <reading-time :content="article.body" />
+          </span>
         </span>
       </span>
+      <button v-if="isBookmarked" @click.prevent="bookmark">
+        unBookmark
+      </button>
+      <button v-else @click.prevent="bookmark">
+        Bookmark
+      </button>
     </router-link>
   </router-link>
 </template>
 
 <script>
 import moment from "moment";
+import { mapActions } from "vuex";
+import { create, remove } from "@/store/bookmark/api";
 
 import ReadingTime from "./ReadingTime";
 
@@ -50,9 +60,33 @@ export default {
   name: "article-item",
   props: ["article"],
   components: { ReadingTime },
+  data() {
+    return {
+      isBookmarked: this.article.bookmarked
+    };
+  },
   computed: {
     createdAt() {
       return moment(this.article.createdAt).format("MMM YY");
+    }
+  },
+  methods: {
+    ...mapActions({
+      bookmark: "bookmark/bookmark",
+      unbookmark: "bookmark/unbookmark"
+    }),
+    async bookmark() {
+      const slug = this.article.slug;
+
+      if (this.isBookmarked) {
+        await remove(slug);
+        this.isBookmarked = false;
+
+        return;
+      }
+
+      await create(slug);
+      this.isBookmarked = true;
     }
   }
 };
--- src/store/bookmark/api.js
+++ src/store/bookmark/api.js
@@ -1,8 +1,32 @@
 import axios from "axios";
 import store from "@/store";
 
+export const create = async slug => {
+  const { data } = await axios.post(
+    `bookmarks`,
+    { articleSlug: slug },
+    {
+      headers: {
+        Authorization: `Bearer ${store.state.auth.access_token}`
+      }
+    }
+  );
+
+  return data;
+};
+
 export const getAll = async () => {
   const { data } = await axios.get(`bookmarks`, {
+    headers: {
+      Authorization: `Bearer ${store.state.auth.access_token}`
+    }
+  });
+
+  return data;
+};
+
+export const remove = async slug => {
+  const { data } = await axios.delete(`bookmarks/${slug}`, {
     headers: {
       Authorization: `Bearer ${store.state.auth.access_token}`
     }
