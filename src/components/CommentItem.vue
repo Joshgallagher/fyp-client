1a81ee0bb65be6a6b4702ebbba5ee7e1aebf9139
Josh <joshgallagher24@gmail.com>
2020-07-23 07:17:00 +0100

    Only show CRUD options is comment is owned by auth user

--- src/components/CommentItem.vue
+++ src/components/CommentItem.vue
@@ -11,8 +11,24 @@
         <time :datetime="comment.createdAt" class="m text-sm text-gray-700 mb-2"
           >Posted {{ createdAt }}</time
         >
-        <p>{{ comment.comment }}</p>
+        <p>{{ this.userComment }}</p>
       </span>
+    </div>
+    <div
+      v-if="authUser.sub === comment.userId"
+      class="flex flex-row justify-end my-4"
+    >
+      <button
+        @click="update"
+        class="mr-4 px-2 py-1 bg-teal-400 text-white rounded font-bold hover:bg-teal-600 transition linear duration-500"
+      >
+        Update
+      </button>
+      <button
+        class="px-2 py-1 bg-red-600 text-white rounded font-bold hover:bg-red-800 transition linear duration-500"
+      >
+        Delete
+      </button>
     </div>
     <hr class="border-gray-200 w-full" />
   </div>
@@ -20,6 +36,7 @@
 
 <script>
 import moment from "moment";
+import { mapGetters } from "vuex";
 
 export default {
   name: "comment-item",
@@ -29,9 +46,23 @@ export default {
       required: true
     }
   },
+  data() {
+    return {
+      updating: false,
+      userComment: this.comment.comment
+    };
+  },
   computed: {
+    ...mapGetters({
+      authUser: "auth/oidcUser"
+    }),
     createdAt() {
       return moment(this.comment.createdAt).fromNow();
+    }
+  },
+  methods: {
+    update() {
+      this.updating = true;
     }
   }
 };
